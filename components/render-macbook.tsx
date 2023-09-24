a40edb40bb03a518e916b79a75be745289f49578
mkafonso <makaya2afonso@gmail.com>
2023-09-24 02:54:07 +0000

    last touches

--- components/render-macbook.tsx
+++ components/render-macbook.tsx
@@ -4,8 +4,10 @@ import {
   ContactShadows,
   Environment,
   Float,
+  Html,
   OrbitControls,
   PresentationControls,
+  Text,
   useGLTF,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
@@ -15,15 +17,34 @@ function MeshComponent() {
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf";
   const gltf = useGLTF(fileURL);
 
-  return <primitive object={gltf.scene} position-y={-1.2} />;
+  return (
+    <primitive object={gltf.scene} position-y={-1.2} rotation-x={0.13}>
+      <Html
+        transform
+        wrapperClass="htmlScreen"
+        distanceFactor={1.17}
+        position={[0, 1.56, -1.4]}
+        rotation-x={-0.256}
+        style={{ pointerEvents: "auto" }}
+      >
+        <iframe src="https://laborit.com.br/"></iframe>
+      </Html>
+    </primitive>
+  );
 }
 
 export function RenderMacbook() {
   return (
     <div className="bg-zinc-800 h-screen w-screen">
-      <Canvas className="h-2xl w-2xl">
+      <Canvas>
         <Environment preset="sunset" />
-        <OrbitControls makeDefault />
+        <OrbitControls
+          makeDefault
+          minPolarAngle={0.1}
+          maxPolarAngle={Math.PI / 2 - 0.1}
+          minAzimuthAngle={-Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+        />
 
         <PresentationControls
           global
@@ -34,6 +55,23 @@ export function RenderMacbook() {
           snap={{ mass: 4, tension: 400 }}
         >
           <Float rotationIntensity={0.4}>
+            <rectAreaLight
+              color="#fafafa"
+              width={2.5}
+              height={1.65}
+              intensity={65}
+              rotation={[-0.1, Math.PI, 0]}
+              position={[0, 0.55, -1.15]}
+            />
+
+            <Text
+              fontSize={0.6}
+              position={[2, 0.75, 0.75]}
+              rotation-y={-1.25}
+              children={`Laborit\nBrasil`}
+              textAlign="center"
+            />
+
             <MeshComponent />
           </Float>
         </PresentationControls>
